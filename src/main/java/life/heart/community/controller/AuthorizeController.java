4efc3d028699b1326151b229749504da71859d34
SeaItFover <3140678431@qq.com>
2020-03-27 09:52:35 +0800

    flyway mysql migrate

--- src/main/java/life/heart/community/controller/AuthorizeController.java
+++ src/main/java/life/heart/community/controller/AuthorizeController.java
@@ -11,7 +11,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.util.UUID;
 
 @Controller
@@ -35,7 +37,8 @@ public class AuthorizeController {
     @GetMapping("/callback")
     public String callback(@RequestParam("code") String code,
                            @RequestParam("state") String state,
-                           HttpServletRequest request){
+                           HttpServletRequest request,
+                           HttpServletResponse response) {
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
         accessTokenDTO.setClient_id(clientId);
         accessTokenDTO.setClient_secret(clientSecret);
@@ -45,19 +48,21 @@ public class AuthorizeController {
         String accessToken = githubProvider.getAccessToken(accessTokenDTO);
         System.out.println(accessToken);
         GithubUser githubUser = githubProvider.getUser(accessToken);
-        if (githubUser != null){
+        if (githubUser != null) {
             //获取到githubuser的时候，将其存入数据库：也就是spring boot 整合mybatis
             User user = new User();
-            user.setToken(UUID.randomUUID().toString());
+            String token = UUID.randomUUID().toString();
+            user.setToken(token);
             user.setName(githubUser.getName());
             user.setAccountId(String.valueOf(githubUser.getId()));
             user.setGmtCreate(System.currentTimeMillis());
             user.setGmtModified(user.getGmtCreate());
             userMapper.insert(user);
-            request.getSession().setAttribute("user",githubUser);
-            return "redirect:/";
             //登陆成功，写cookie 和session
-        }else{
+            response.addCookie(new Cookie("token",token));
+            //request.getSession().setAttribute("user", githubUser);
+            return "redirect:/";
+        } else {
             return "redirect:/";
         }
 
--- src/main/java/life/heart/community/controller/IndexController.java
+++ src/main/java/life/heart/community/controller/IndexController.java
@@ -1,15 +1,48 @@
 package life.heart.community.controller;
 
+import life.heart.community.mapper.UserMapper;
+import life.heart.community.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+
 @Controller
 public class IndexController {
 
+    @Autowired
+    private UserMapper userMapper;
+
+    /**
+     * 当访问首页的时候，循环去看所有的cookie，找到名字是token的cookie
+     * 然后去数据库中查是不是有cookie这条记录，如果有，就把user放大session
+     * 里面。
+     *
+     * @param request
+     * @return
+     */
     @GetMapping("/")
-    public String index(){
+    public String index(HttpServletRequest request) {
+        Cookie[] cookies = request.getCookies();
+        if (cookies != null) {
+            for (Cookie cookie : cookies) {
+                if (cookie.equals("token")) {
+                    System.out.println("cookie is not null");
+                    String token = cookie.getValue();
+                    System.out.println("token = " + token);
+                    User user = userMapper.findByToken(token);
+                    if (user != null) {
+                        System.out.println("user isnot null");
+                        request.getSession().setAttribute("user", user);
+                    }
+                    break;
+                }
+            }
+        }
         //model.addAttribute("name",name);
         return "index";
     }
