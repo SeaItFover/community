61919c8f7a17b3adb358af18c011efc7778a711e
SeaItFover <3140678431@qq.com>
2020-03-28 23:06:45 +0800

    pagination and module fragment

--- src/main/java/life/heart/community/controller/IndexController.java
+++ src/main/java/life/heart/community/controller/IndexController.java
@@ -1,7 +1,12 @@
 package life.heart.community.controller;
 
+import life.heart.community.dto.PaginationDTO;
+import life.heart.community.dto.QuestionDTO;
+import life.heart.community.mapper.QuestionMapper;
 import life.heart.community.mapper.UserMapper;
+import life.heart.community.model.Question;
 import life.heart.community.model.User;
+import life.heart.community.service.QuestionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -10,12 +15,16 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
+import java.util.List;
 
 @Controller
 public class IndexController {
 
     @Autowired
     private UserMapper userMapper;
+
+    @Autowired
+    private QuestionService questionService;
 
     /**
      * 当访问首页的时候，循环去看所有的cookie，找到名字是token的cookie
@@ -26,24 +35,26 @@ public class IndexController {
      * @return
      */
     @GetMapping("/")
-    public String index(HttpServletRequest request) {
+    public String index(HttpServletRequest request,
+                        Model model,
+                        @RequestParam(name = "page", defaultValue = "1") Integer page,
+                        @RequestParam(name = "size", defaultValue = "5") Integer size) {
         Cookie[] cookies = request.getCookies();
         if (cookies != null) {
             for (Cookie cookie : cookies) {
                 if (cookie.getName().equals("token")) {
-                    System.out.println("cookie is not null");
                     String token = cookie.getValue();
-                    System.out.println("token = " + token);
                     User user = userMapper.findByToken(token);
                     if (user != null) {
-                        System.out.println("user isnot null");
                         request.getSession().setAttribute("user", user);
                     }
                     break;
                 }
             }
         }
-        //model.addAttribute("name",name);
+
+        PaginationDTO pagination = questionService.list(page,size);
+        model.addAttribute("pagination", pagination);
         return "index";
     }
 }
