e2f9805b3aa69c63b90bdd8851fd6728263d907c
SeaItFover <3140678431@qq.com>
2020-03-30 20:15:57 +0800

    profile and question

---
+++ src/main/java/life/heart/community/controller/QuestionController.java
@@ -0,0 +1,25 @@
+package life.heart.community.controller;
+
+import life.heart.community.dto.QuestionDTO;
+import life.heart.community.mapper.QuestionMapper;
+import life.heart.community.service.QuestionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Controller
+public class QuestionController {
+
+    @Autowired
+    private QuestionService questionService;
+
+    @GetMapping("/question/{id}")
+    public String question(@PathVariable(name = "id") Integer id,
+                           Model model) {
+        QuestionDTO questionDTO = questionService.getById(id);
+        model.addAttribute("question",questionDTO);
+        return "question";
+    }
+}
