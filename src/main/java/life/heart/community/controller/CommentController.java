fd0f51e3710c111f96d3c802707143c96c0b5cb0
SeaItFover <3140678431@qq.com>
2020-04-02 20:01:07 +0800

    list of reply

--- src/main/java/life/heart/community/controller/CommentController.java
+++ src/main/java/life/heart/community/controller/CommentController.java
@@ -1,6 +1,6 @@
 package life.heart.community.controller;
 
-import life.heart.community.dto.CommentDTO;
+import life.heart.community.dto.CommentCreateDTO;
 import life.heart.community.dto.ResultDTO;
 import life.heart.community.exception.CustomizeErrorCode;
 import life.heart.community.mapper.CommentMapper;
@@ -27,7 +27,7 @@ public class CommentController {
 
     @ResponseBody
     @RequestMapping(value = "/comment",method = RequestMethod.POST)
-    public Object post(@RequestBody CommentDTO commentDTO,
+    public Object post(@RequestBody CommentCreateDTO commentCreateDTO,
                        HttpServletRequest request) {
 
         User user = (User)request.getSession().getAttribute("user");
@@ -35,9 +35,9 @@ public class CommentController {
             return ResultDTO.errorOf(CustomizeErrorCode.NO_LOGIN);
         }
         Comment comment = new Comment();
-        comment.setParentId(commentDTO.getParentId());
-        comment.setContent(commentDTO.getContent());
-        comment.setType(commentDTO.getType());
+        comment.setParentId(commentCreateDTO.getParentId());
+        comment.setContent(commentCreateDTO.getContent());
+        comment.setType(commentCreateDTO.getType());
         comment.setGmtModified(System.currentTimeMillis());
         comment.setGmtCreate(System.currentTimeMillis());
         comment.setCommentator(user.getId());
--- src/main/java/life/heart/community/service/CommentService.java
+++ src/main/java/life/heart/community/service/CommentService.java
@@ -1,16 +1,21 @@
 package life.heart.community.service;
 
+import life.heart.community.dto.CommentDTO;
 import life.heart.community.enums.CommentTypeEnum;
 import life.heart.community.exception.CustomizeErrorCode;
 import life.heart.community.exception.CustomizeException;
 import life.heart.community.mapper.CommentMapper;
 import life.heart.community.mapper.QuestionExtMapper;
 import life.heart.community.mapper.QuestionMapper;
-import life.heart.community.model.Comment;
-import life.heart.community.model.Question;
+import life.heart.community.mapper.UserMapper;
+import life.heart.community.model.*;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.*;
+import java.util.stream.Collectors;
 
 @Service
 public class CommentService {
@@ -23,6 +28,9 @@ public class CommentService {
 
     @Autowired
     private QuestionExtMapper questionExtMapper;
+
+    @Autowired
+    private UserMapper userMapper;
 
     @Transactional
     public void insert(Comment comment) {
@@ -50,5 +58,35 @@ public class CommentService {
             question.setCommentCount(1);
             questionExtMapper.incCommentCount(question);
         }
+    }
+
+    public List<CommentDTO> listByQuestionId(Long id) {
+        CommentExample commentExample = new CommentExample();
+        commentExample.createCriteria()
+                .andParentIdEqualTo(id)
+                .andTypeEqualTo(CommentTypeEnum.QUESTION.getType());
+        List<Comment> comments = commentMapper.selectByExample(commentExample);
+
+        if (comments.size() == 0){
+            return new ArrayList<>();
+        }
+        Set<Long> commentators = comments.stream().map(comment -> comment.getCommentator()).collect(Collectors.toSet());
+        List<Long> userIds = new ArrayList<>();
+        userIds.addAll(commentators);
+
+        UserExample userExample = new UserExample();
+        userExample.createCriteria()
+                .andIdIn(userIds);
+        List<User> users = userMapper.selectByExample(userExample);
+        Map<Long, User> userMap = users.stream().collect(Collectors.toMap(user -> user.getId(), user -> user));
+
+        List<CommentDTO> commentDTOS = comments.stream().map(comment -> {
+            CommentDTO commentDTO = new CommentDTO();
+            BeanUtils.copyProperties(comment,commentDTO);
+            commentDTO.setUser(userMap.get(comment.getCommentator()));
+            return commentDTO;
+        }).collect(Collectors.toList());
+
+        return commentDTOS;
     }
 }
