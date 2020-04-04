9ddd722640a30dccf03b04e2920dc1badb60b7b8
SeaItFover <3140678431@qq.com>
2020-04-04 10:24:39 +0800

    old commit

--- src/main/java/life/heart/community/controller/CommentController.java
+++ src/main/java/life/heart/community/controller/CommentController.java
@@ -7,6 +7,7 @@ import life.heart.community.mapper.CommentMapper;
 import life.heart.community.model.Comment;
 import life.heart.community.model.User;
 import life.heart.community.service.CommentService;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -33,6 +34,10 @@ public class CommentController {
         User user = (User)request.getSession().getAttribute("user");
         if (user == null){
             return ResultDTO.errorOf(CustomizeErrorCode.NO_LOGIN);
+        }
+
+        if (commentCreateDTO == null || StringUtils.isBlank(commentCreateDTO.getContent())) {
+            return ResultDTO.errorOf(CustomizeErrorCode.CONTENT_IS_EMPTY);
         }
         Comment comment = new Comment();
         comment.setParentId(commentCreateDTO.getParentId());
--- src/main/java/life/heart/community/exception/CustomizeErrorCode.java
+++ src/main/java/life/heart/community/exception/CustomizeErrorCode.java
@@ -7,7 +7,8 @@ public enum  CustomizeErrorCode implements ICustomizeErrorCode {
     NO_LOGIN(2003,"当前操作需要登陆，请登陆后重试"),
     SYSTEM_ERROR(2004,"冒烟了冒烟了，一会再来吧！"),
     TYPE_PARAM_WRONG(2005,"评论类型错误或不存在！"),
-    COMMENT_NOT_FOUND(2006,"回复的评论不存在！"),;
+    COMMENT_NOT_FOUND(2006,"回复的评论不存在！"),
+    CONTENT_IS_EMPTY(2007,"输入内容不能为空"),;
     @Override
     public String getMessage() {
         return message;
